256636a0dccad8c07c4c71b710233b31c93269d7
MichPlewa <michalplewa02@gmail.com>
2023-07-31 18:29:34 +0200

    Background

--- axe-game.cpp
+++ axe-game.cpp
@@ -1,4 +1,11 @@
 #include <raylib.h>
 int main(){
-    
-}
+    int width = 350;
+    int height = 200;
+    InitWindow(width, height, "My Window");
+    while(true){
+        BeginDrawing();
+        ClearBackground(RED);
+        EndDrawing();
+    }
+};
