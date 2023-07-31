673397c8fb8eac5060a36331ad370014a9a1d7f3
MichPlewa <michalplewa02@gmail.com>
2023-07-31 19:46:07 +0200

    Add Circle

--- axe-game.cpp
+++ axe-game.cpp
@@ -2,10 +2,27 @@
 int main(){
     int width = 350;
     int height = 200;
+    int circleA = 175;
+    int circleB = 100;
+
     InitWindow(width, height, "My Window");
-    while(true){
+
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose()){
         BeginDrawing();
-        ClearBackground(RED);
+
+        ClearBackground(WHITE);
+
+        DrawCircle(circleA, circleB, 25, BLUE);
+
+        if(IsKeyDown(KEY_D)){
+            circleA += 10;
+        };
+        if(IsKeyDown(KEY_A)){
+            circleA -= 10;
+        };
+
         EndDrawing();
     }
 };
